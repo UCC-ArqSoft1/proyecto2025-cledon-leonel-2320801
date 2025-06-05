cd3fd553537cd567698723c6441a503e57fe3994
LeonelCledon <2320801@ucc.edu.ar>
2025-06-05 00:42:24 -0300

    Dockerización del proyecto final

--- backend/main.go
+++ backend/main.go
@@ -36,7 +36,7 @@ func main() {
 
 	// Configurar CORS
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowOrigins: []string{"http://localhost:3000", "http://localhost", "http://localhost:80"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
